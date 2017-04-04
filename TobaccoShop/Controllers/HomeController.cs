e1b72912cd7ca166b3f7c9d14d13a149f97de9cc
Garik <maximgagarinov93@gmail.com>
2017-04-04 22:02:06 +0300

    First Commit

--- TobaccoShop/Controllers/HomeController.cs
+++ TobaccoShop/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace TobaccoShop.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            int l = 0;
             return View();
         }
 
